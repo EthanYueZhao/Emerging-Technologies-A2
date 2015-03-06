dbcae99ec0a8c2b0d5cfd54af436580dc72485d1
Ethan Yue Zhao <xishanlake@gmail.com>
2015-03-06 15:59:27 -0500

    added some dependencies

--- EmergingA2/public/scripts/chat.js
+++ EmergingA2/public/scripts/chat.js
@@ -1,2 +1,12 @@
 ﻿console.log("This is char.js");
 var socket = io();
+'use strict';
+var chatApp = angular.modual('chatApp', []);
+
+
+
+chatApp.controller('chatCtrl', ['$scope', 'socket'],
+    function ($scope, socket)
+{
+     
+});
