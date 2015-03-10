e304352ea73580a7fc3dbb870ed3450270cb974a
Ethan Yue Zhao <xishanlake@gmail.com>
2015-03-10 12:47:49 -0400

    add angular route function

--- EmergingA2/public/scripts/chat.js
+++ EmergingA2/public/scripts/chat.js
@@ -1,23 +1,47 @@
 ﻿'use strict';
-var chatApp = angular.module('chatApp', []);
+// main app module
+var chatApp = angular.module('chatApp', ['ngRoute', 'chatAppControllers']);
 
-chatApp.factory('socket', function () {
+// app route config
+chatApp.config(['$routeProvider', 
+    function ($routeProvider)
+    {
+      $routeProvider.
+      when('/chat', {
+            templateUrl: '/partial/chat.html',
+            controller: 'chatCtrl'
+        }).
+      otherwise({
+            redirectTo: '/chat'            
+        });
+    }]);
+
+// get socket service
+chatApp.factory('socket', function ()
+{
     var socket = io.connect('http://localhost:3000');
     return socket;
 });
 
-chatApp.controller('chatCtrl', ['$scope', 'socket',
-    function ($scope, socket) {
-        $scope.msgs = [];
-        $scope.sendMsg = function () {
-            var clientMsg =  $scope.msg.text + '     sent time: ' + new Date().toString();
-            socket.emit('send message', clientMsg);
-            $scope.msgs.push(clientMsg);
-            $scope.msg.text = '';
-        };
+// app controllers module
+var chatAppControllers = angular.module('chatAppControllers', []);
 
-        socket.on('get message', function (data) {
-            $scope.msgs.push(data);
-            $scope.$digest();
+// chat partial controller
+chatAppControllers.controller('chatCtrl', ['$scope', 'socket',
+    function ($scope, socket)
+    {
+        $scope.msgs = [];
+        $scope.sendMsg = function ()
+        {
+            var clientMsg = $scope.msg.text;
+            socket.emit('send message', clientMsg); // emit message to server
+            $scope.msgs.push(clientMsg + '     sent time: ' + new Date().toString()); // add message to local array
+            $scope.msg.text = ''; // clear input box
+        };
+        
+        socket.on('get message', function (data)
+        {
+            $scope.msgs.push(data); // add message emitted from server to local array
+            $scope.$digest(); // refresh local array
         })
     }]);
